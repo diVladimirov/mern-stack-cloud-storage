7126f7c5a3797d246b06dc189313a53815c179cc
Vadim Vladimirov <kurtonchik@gmail.com>
2022-06-16 22:20:27 +0300

    add dir and file svg

--- client/src/components/Disk/File/File.js
+++ client/src/components/Disk/File/File.js
@@ -1,12 +1,13 @@
 import React from "react";
+import dirLogo from "../../../assets/img/iconmonstr-folder-15.svg";
+import fileLogo from "../../../assets/img/iconmonstr-file-2.svg";
 
 const File = ({ file }) => {
   return (
     <div>
-      File
-      {/* <img src="" alt /> */}
+      <img src={file.type === "dir" ? dirLogo : fileLogo} alt="" />
       <div>{file.name}</div>
-      <div>{file.date}</div>
+      <div>{file.date.slice(0, 10)}</div>
       <div>{file.size}</div>
     </div>
   );
